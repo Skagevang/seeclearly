56699a07a705fa13236af7e83d01231155d96c2b
Sigurd Aasbrenn <sigurd.aasbrenn@gmail.com>
2017-10-12 14:45:08 +0200

    added arrows to the faq categories

--- script.js
+++ script.js
@@ -6,15 +6,22 @@ for (let anchor of anchors) {
   }
 }
 
-//FAQ - show/hide the answer part of the categories
+//FAQ - show/hide the answer part of the "questions" and changes arrow direction
 
-function toggle_visibility(answer_div) {
-	let a = document.getElementById(answer_div);
-	if (a.style.display === "block") {
-		a.style.display = "none";
+function toggle_visibility(button, answer_div) {
+	let div = document.getElementById(answer_div);
+	let but = document.getElementById(button);
+	let text_with_arrow = but.innerHTML;
+	text = text_with_arrow.substr(0, text_with_arrow.length -16);
+
+
+	if (div.style.display === "block") {
+		div.style.display = "none";
+		but.innerHTML = text + "<arrow>▼</arrow>";
 	}
 	else {
-		a.style.display = "block";
+		div.style.display = "block";
+		but.innerHTML = text + "<arrow>▲</arrow>";
 	}
 }
 
