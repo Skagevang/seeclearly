4123de5928fde6406059e156e729e04fb00e3f28
Ole-Martin Skagevang <hero3@hotmail.com>
2017-10-25 18:14:01 +0200

    fixed contact and everything

--- script.js
+++ script.js
@@ -3,5 +3,12 @@ const anchors = [...document.getElementsByTagName('a')];
 for (let anchor of anchors) {
   if (anchor.href === window.location.href) {
     anchor.style.backgroundColor = 'green';
+    anchor.style.color = 'white';
+  }
+  else if (window.location.href.indexOf('about') !== -1) {
+    if (anchor.href.indexOf('about.html') !== -1) {
+      anchor.style.backgroundColor = 'green';
+      anchor.style.color = 'white';
+    }
   }
 }
